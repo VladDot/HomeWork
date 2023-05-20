824b92b591d7a22ec224c4a9888e969a590fa571
Vlad Kostirya <vladkostirya4@gmail.com>
2023-05-20 15:02:31 +0300

    m

--- movie/src/js/main.js
+++ movie/src/js/main.js
@@ -1,1 +1,12 @@
-export const {API_KEY} = process.env;
+export const { API_KEY } = process.env;
+console.log("hellffo");
+
+const options = { method: "GET", headers: { accept: "application/json" } };
+
+fetch(
+  "https://api.themoviedb.org/3/movie/popular?api_key=a23b245444dfc6a40ca216bd5ab2fab6",
+  options
+)
+  .then((response) => response.json())
+  .then((response) => console.log(response))
+  .catch((err) => console.error(err));
